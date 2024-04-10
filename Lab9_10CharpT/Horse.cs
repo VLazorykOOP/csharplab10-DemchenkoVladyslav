3418a32cc45d1806a6cc6b315b33eaa47687c488
DemchenkoVladyslav <demchenkovlad80@gmail.com>
2024-04-10 11:39:26 +0300

    tasks completed

---
+++ Lab9_10CharpT/Horse.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9_10CharpT
+{
+    public class Horse
+    {
+        private string name;
+        private int age;
+
+        // Подія, що відбувається, коли кінь народжується
+        public event EventHandler Born;
+
+        // Подія, що відбувається, коли кінь відомляє про свій вік
+        public event EventHandler AgeAnnounced;
+
+        public Horse(string name)
+        {
+            this.name = name;
+        }
+
+        // Метод, який викликається, коли кінь народжується
+        public void Birth()
+        {
+            Console.WriteLine($"{name} народився!");
+            OnBorn();
+        }
+
+        // Метод, який викликається, коли кінь відомляє про свій вік
+        public void AnnounceAge(int age)
+        {
+            this.age = age;
+            Console.WriteLine($"{name} має {age} років.");
+            OnAgeAnnounced();
+        }
+
+        // Метод для виклику події Born
+        protected virtual void OnBorn()
+        {
+            Born?.Invoke(this, EventArgs.Empty);
+        }
+
+        // Метод для виклику події AgeAnnounced
+        protected virtual void OnAgeAnnounced()
+        {
+            AgeAnnounced?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Lab9_10CharpT/NotReversedStringException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9_10CharpT
+{
+    public class NotReversedStringException : Exception
+    {
+        public NotReversedStringException() : base("This string is not reversed") { }
+        public NotReversedStringException(string message) : base(message) { }
+    }
+}
